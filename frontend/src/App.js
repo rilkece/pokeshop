3e65f22841859d7d1fa0b1d9ad74a3896602d986
Erickle de Lucena Leite <erickle.luc@gmail.com>
2022-07-27 00:51:42 +0100

    Pokeshop-front-created

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">PokePet</a>
+      </header>
+      <main>
+        List products
+      </main>
+    </div>
+  );
+}
+
+export default App;
